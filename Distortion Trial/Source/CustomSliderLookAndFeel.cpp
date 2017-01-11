ffb08c7578a840586ab682357298bacef803405a
Max Marchionda <maxmarchionda@Maxs-MacBook-Pro.local>
2017-01-10 22:16:57 -0600

    adding lookandfeel file

---
+++ Distortion Trial/Source/CustomSliderLookAndFeel.cpp
@@ -0,0 +1,112 @@
+/*
+  ==============================================================================
+
+    CustomSliderLookAndFeel.cpp
+    Created: 10 Jan 2017 9:41:28pm
+    Author:  Max Marchionda
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class SliderLook : public LookAndFeel_V3
+{
+public:
+    SliderLook()
+    {
+        //setColour(Slider::textBoxBackgroundColourId, Colours::red);
+    }
+    
+    void drawRotarySlider(Graphics& g, int x, int y, int width, int height, float sliderPos,
+                          const float rotaryStartAngle, const float rotaryEndAngle, Slider& slider) override
+    {
+        const float radius = jmin(width / 2, height / 2) - 2.0f;
+        const float centreX = x + width * 0.5f;
+        const float centreY = y + height * 0.5f;
+        const float rx = centreX - radius;
+        const float ry = centreY - radius;
+        const float rw = radius * 2.0f;
+        const float angle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
+        const bool isMouseOver = slider.isMouseOverOrDragging() && slider.isEnabled();
+        
+        if (radius > 12.0f)
+        {
+            const float thickness = 0.7f;
+            const float insideThickness = 0.96f;
+            
+            if (slider.isEnabled())
+                g.setColour(slider.findColour(Slider::rotarySliderFillColourId).withAlpha(isMouseOver ? 1.0f : 0.9f));
+                else
+                    g.setColour(Colour(0x80808080));
+                    
+                    
+                {
+                    Path filledArc;
+                    filledArc.addPieSegment(rx, ry, rw, rw, rotaryStartAngle, angle, thickness);
+                    g.fillPath(filledArc);
+                }
+            /*
+             {
+             const float innerRadius = radius * 0.2f;
+             Path p;
+             p.addTriangle(-innerRadius, 0.0f,
+             0.0f, -radius * thickness * 1.1f,
+             innerRadius, 0.0f);
+             
+             p.addEllipse(-innerRadius, -innerRadius, innerRadius * 2.0f, innerRadius * 2.0f);
+             
+             g.fillPath(p, AffineTransform::rotation(angle).translated(centreX, centreY));
+             }*/
+            
+            if (slider.isEnabled())
+                g.setColour(slider.findColour(Slider::rotarySliderOutlineColourId));
+                else
+                    g.setColour(Colour(0x80808080));
+                /*
+                 Path outlineArc;
+                 outlineArc.addPieSegment(rx, ry, rw, rw, rotaryStartAngle, rotaryEndAngle, thickness);
+                 outlineArc.closeSubPath();
+                 g.strokePath(outlineArc, PathStrokeType(slider.isEnabled() ? (isMouseOver ? 2.0f : 1.2f) : 0.3f));
+                 */
+                    
+                    g.setColour(Colours::grey);
+                    Path insideArc;
+            insideArc.addPieSegment(rx, ry, rw, rw, rotaryStartAngle, rotaryEndAngle, insideThickness);
+            g.fillPath(insideArc);
+            
+            
+            //insideArc.closeSubPath();
+            
+            
+            
+            
+        }
+        else
+        {
+            if (slider.isEnabled())
+                g.setColour(slider.findColour(Slider::rotarySliderFillColourId).withAlpha(isMouseOver ? 1.0f : 0.7f));
+                else
+                    g.setColour(Colour(0x80808080));
+                    
+                    Path p;
+            p.addEllipse(-0.4f * rw, -0.4f * rw, rw * 0.8f, rw * 0.8f);
+            PathStrokeType(rw * 0.1f).createStrokedPath(p, p);
+            
+            p.addLineSegment(Line<float>(0.0f, 0.0f, 0.0f, -radius), rw * 0.2f);
+            
+            g.fillPath(p, AffineTransform::rotation(angle).translated(centreX, centreY));
+        }
+    }
+    /*
+     Slider::SliderLayout getSliderLayout(Slider&amp) override
+     {
+     Slider::SliderLayout layout;
+     
+     layout.textBoxBounds = Rectangle<int>(15, 35, 70, 20);
+     layout.sliderBounds = Rectangle<int> (0, 0, 100, 100);
+     
+     return layout;
+     }*/
+};
+
